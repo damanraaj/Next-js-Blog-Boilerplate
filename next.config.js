08740943abc4ce1f218adc80be21b881514a54c9
Daman Raaj <40737621+damanraaj@users.noreply.github.com>
2023-12-03 01:25:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -25,6 +25,10 @@ module.exports = withBundleAnalyzer({
   env: {
     baseUrl,
   },
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
